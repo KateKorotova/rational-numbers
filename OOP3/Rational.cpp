91a704ac111862e36f7152330abe863176ad6e90
katya_000 <katya_000@KATEKOROTOVA>
2017-11-28 21:28:55 +0200

    operators +,-

--- OOP3/Rational.cpp
+++ OOP3/Rational.cpp
@@ -1,14 +1,15 @@
 #include "Rational.h"
+#include <math.h>
 
 Rational::Rational(){
 	(*this).sign = 1;
 }
+
 Rational::Rational(const Rational& other) {
 	(*this).sign = other.sign;
 	(*this).ch = other.ch;
 	(*this).zn = other.zn;
 }
-
 
 Rational::Rational(long long ch, long long zn, int sign) {
 	(*this).zn = zn;
@@ -30,11 +31,12 @@ Rational Rational::reduction() {
 	Rational ans = (*this);
 	long long g = gcd(ans.ch, ans.zn);
 	if (g != 1) {
-		ans.ch /= g;
-		ans.zn /= g;
+		ans.ch /= abs(g);
+		ans.zn /= abs(g);
 	}
 	return ans; 
 }
+
 Rational Rational::reduction_(const long long &a, const long long &b) {
 	Rational ans; 
 	ans.ch = a;
@@ -47,28 +49,72 @@ Rational Rational::reduction_(const long long &a, const long long &b) {
 	return ans; 
 }
 
-//Rational Rational::operator +(const Rational& b) {
-//	Rational res;
-//	if ((*this).sign == b.sign) {
-//		res.zn = gcd((*this).zn, b.zn);
-//		res.ch = (*this).ch*res.zn + b.ch*res.zn;
-//		res.sign = (*this).sign;
-//		res = res.reduction();
-//		return res;
-//	}
-//
-//}
-//Rational Rational::operator -(const Rational& b) {
-//	Rational res;
-//
-//}
+Rational Rational::operator -()const {
+	Rational ans = *this;
+	ans.sign = (-1)*(*this).sign;
+	return ans;
+}
+
+Rational Rational::add_module(const Rational& b) {
+	Rational res;
+	res.zn = (zn / gcd((*this).zn, b.zn))*b.zn;
+	res.ch = ((res.zn / (*this).zn)*(*this).ch) + ((res.zn / b.zn)*b.ch);
+	res = res.reduction();
+	return res;
+}
+
+Rational Rational::sub_module(const Rational& b) {
+	Rational res;
+	res.zn = (zn / gcd((*this).zn, b.zn))*b.zn;
+	res.ch = ((res.zn / (*this).zn)*(*this).ch) - ((res.zn / b.zn)*b.ch);
+	res = res.reduction();
+	return res;
+}
+
+int Rational::cmp_module(const Rational& b) {
+	Rational res;
+	res.zn = (zn / gcd((*this).zn, b.zn))*b.zn;
+	int cmp1 = (res.zn / (*this).zn)*(*this).ch;
+	int cmp2 = (res.zn / b.zn)*b.ch;
+	if (cmp1 > cmp2)
+		return 1;
+	if (cmp1 == cmp2)
+		return 0;
+	if (cmp1 < cmp2)
+		return -1;
+}
+
+Rational Rational::operator +(const Rational& b) {
+	Rational res;
+	if (sign == b.sign) {
+		res = (*this).add_module(b);
+		res.sign = (*this).sign;
+	}
+	else{
+		Rational a(b);
+		if ((*this).cmp_module(b) == 1) {
+			res = (*this).sub_module(b);
+			res.sign = sign;
+		}
+		else
+			if ((*this).cmp_module(b) == -1) {
+				res = a.sub_module((*this));
+				res.sign = b.sign;
+			}
+			else
+				res.ch = 0;
+	}
+	return res;
+}
+
+Rational Rational::operator -(const Rational& b) {
+	return *this + (-b); 
+}
 
 Rational Rational::operator *(const Rational& b) {
 	Rational res;
-	Rational f;
-	f = (*this);
-	Rational s;
-	s = b;
+	Rational f = (*this);
+	Rational s = b;
 	f = reduction_((*this).ch, b.zn);
 	s = reduction_(b.ch, (*this).zn);
 	res.ch = f.ch*s.ch;
@@ -77,9 +123,14 @@ Rational Rational::operator *(const Rational& b) {
 	res = res.reduction();
 	return res;
 }
-//Rational Rational::operator /(const Rational& b) {
-//
-//}
+
+Rational Rational::operator /(const Rational& b){
+	Rational rev = b;
+	long long tmp = rev.ch;
+	rev.ch = rev.zn;
+	rev.zn = tmp;
+	return rev*(*this);
+}
 
 bool Rational::operator ==(const Rational& b) {
 	return ((*this).zn == b.zn && (*this).ch == b.ch && (*this).sign == b.sign);
@@ -101,12 +152,15 @@ std::istream& operator >> (std::istream& os, Rational& ans) {
 }
 
 std::ostream& operator<< (std::ostream& os, const Rational& num) {
-	if (num.sign == -1)
-		os << "-";
-	os << num.ch << "/" << num.zn;
+	if (num.ch == 0)
+		os << "0";
+	else {
+		if (num.sign == -1 && num.ch > 0)
+			os << "-";
+		os << num.ch << "/" << num.zn;
+	}
 	return os; 
 }
-
 
 int main() {
 	Rational r;
@@ -122,6 +176,10 @@ int main() {
 	else
 		std::cout << "no" << std::endl;
 	std::cout << r1*r2 << std::endl;
+	r = r1 / r2;
+	std::cout << r << std::endl; 	
+	std::cout << r1 + r2 << std::endl;
+	std::cout << r1 - r2 << std::endl;
 
 	std::system("pause");
 }
--- OOP3/Rational.h
+++ OOP3/Rational.h
@@ -7,13 +7,17 @@ private:
 	long long ch;
 	long long zn;
 	long long sign;
+	long long gcd(long long a, long long b);
+	int cmp_module(const Rational& b);
 	Rational reduction();
 	Rational reduction_(const long long &a, const long long &b);
-	long long gcd(long long a,long long b);
+	Rational add_module(const Rational& b);
+	Rational sub_module(const Rational& b);
 public:
 	Rational();
 	Rational(const Rational& other);
 	Rational(long long ch, long long zn, int sign);
+	Rational operator -()const;
 	Rational operator +(const Rational& b);
 	Rational operator -(const Rational& b);
 	Rational operator *(const Rational& b);
